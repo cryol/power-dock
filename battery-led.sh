74fd9ba21683a8ba65c1599c6a00385434f2bcaf
greenbreakfast <lazardemin@gmail.com>
2016-03-17 15:30:53 -0400

    changed pulse to reflect addition of inverter to design; added function for reading the battery level

--- battery-led.sh
+++ battery-led.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
 ReadGpio () {
-        val=$(gpioctl get $1 | grep LOW)
+	val=$(gpioctl get $1 | grep LOW)
 
-        if [ "$val" != "" ]; then
-                ret="0"
-        else
-                ret="1"
-        fi
+	if [ "$val" != "" ]; then
+		ret="0"
+	else
+		ret="1"
+	fi
 
-        echo "$ret"
+	echo "$ret"
+}
+
+CheckBatteryLevel () {
+	charge=$(ReadGpio 16)
+	echo "> Charging status: $charge"
+
+	battery1=$(ReadGpio 17)
+	battery2=$(ReadGpio 15)
+	echo "> Battery Level: $battery2 $battery1"
 }
 
 echo "> Enabling Battery LEDs"
@@ -28,16 +37,14 @@ gpioctl dirin 16 >& /dev/null
 gpioctl dirin 17 >& /dev/null
 
 echo "> Pulse"
-gpioctl dirout-low 19 >& /dev/null
-/usr/bin/sleep 0.1
 gpioctl dirout-high 19 >& /dev/null
+/usr/bin/sleep 0.1
+gpioctl dirout-low 19 >& /dev/null
+
+/usr/bin/sleep 0.5
+CheckBatteryLevel
 
 /usr/bin/sleep 1
-
-charge=$(ReadGpio 16)
-echo "> Charging status: $charge"
-
-battery1=$(ReadGpio 17)
-battery2=$(ReadGpio 15)
-echo "> Battery Level: $battery2 $battery1"
+echo "> Next battery check"
+CheckBatteryLevel
 
