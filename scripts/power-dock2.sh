01c7c611ca2ec61ea5b9e33312cdc58a60fd0dba
greenbreakfast <lazardemin@gmail.com>
2018-04-15 17:41:17 -0400

    Added argument to specify if battery percentage is reported as well (both regular text and json); added a -loop <s> argument to instruct the program to run indefinitely, with <s> seconds between each read; isolated ADC and math related code into their own functions

--- scripts/power-dock2.sh
+++ scripts/power-dock2.sh
@@ -5,6 +5,8 @@
 # global variables
 bUsage=0
 bJson=0
+bReportPercentage=0
+progLoop=0
 
 VbattMax=4.20
 
@@ -16,22 +18,71 @@ Usage () {
 	echo "Onion Power Dock 2: enables the Battery Level Indicator LEDs and outputs the battery voltage"
 	echo ""
 	echo "OPTIONS:"
+	echo " -l <TIME>	Repeatedly report battery level,"
+	echo "   		with <TIME> seconds between each report"
+	echo " -p 		Show battery percentage as well"
 	echo " -j 		json output"
 	echo " -h 		help: show this prompt"
 	echo ""
 }
 
 # find voltage measurement from ADC
-getBatteryVoltage() {
+getRawBatteryVoltage() {
 	# find the ADC measurement
-	adc=$(($(i2cget -y 0 0x4d 0x00 w | sed -e 's/0x\(..\)\(..\)/0x\2\1/')/4))
+	local adc=$(($(i2cget -y 0 0x4d 0x00 w | sed -e 's/0x\(..\)\(..\)/0x\2\1/')/4))
 	#echo $adc
 
-	Ain=$(echo "($adc/1024)*(3.3)" | bc -l)
+	local Ain=$(echo "($adc/1024)*(3.3)" | bc -l)
 	#echo $Ain
 
-	Vbatt=$(echo "$Ain*5/4" | bc -l)
+	local Vbatt=$(echo "$Ain*5/4" | bc -l)
 	echo "$Vbatt"
+}
+
+# return battery voltage measurement with 2 decimal points
+getBatteryVoltage() {
+	local Vbatt=$(getRawBatteryVoltage)
+	local VbattRounded=$(echo "scale=2; $Vbatt/1" | bc -l)
+
+	echo "$VbattRounded"
+}
+
+# return battery percentage out of VbattMax
+getBatteryPercentage() {
+	local Vbatt=$(getRawBatteryVoltage)
+
+	local battDecimal=$(echo "scale=2; $Vbatt/$VbattMax" | bc -l)
+	local battPercent=$(echo "$battDecimal * 100" | bc -l)
+	battPercent=$(echo "scale=0; $battPercent / 1" | bc -l)
+
+	echo "$battPercent"
+}
+
+# read and report battery level
+reportBatteryLevel() {
+	# read the battery level
+	local VbattRounded=$(getBatteryVoltage)
+	if [ $bReportPercentage -ne 0 ]; then
+		local battPercent=$(getBatteryPercentage)
+	fi
+
+	# enable the battery LEDs
+	/usr/bin/power-dock -q2
+
+	# print output
+	if [ $bJson == 1 ]; then
+		echo -n "{\"voltage\":$VbattRounded"
+		if [ $bReportPercentage -ne 0 ]; then
+			echo ", \"percent\":$battPercent}"
+		else
+			echo "}"
+		fi
+	else
+		echo "Battery Voltage Level: $VbattRounded V"
+		if [ $bReportPercentage -ne 0 ]; then
+			echo "Battery Level Percent: $battPercent %"
+		fi
+	fi
 }
 
 
@@ -40,6 +91,15 @@ getBatteryVoltage() {
 while [ "$1" != "" ]
 do
     case "$1" in
+		-l|--l|loop|-loop|--loop)
+			shift
+            progLoop="$1"
+            shift
+        ;;
+		-p|--p|percent|-percent|--percent)
+            bReportPercentage=1
+            shift
+        ;;
         -h|--h|help|-help|--help)
             bUsage=1
             shift
@@ -63,21 +123,14 @@ if [ $bUsage == 1 ]; then
     exit
 fi
 
-## normal operation
-# read the battery level
-Vbatt=$(getBatteryVoltage)
-VbattRounded=$(echo "scale=2; $Vbatt/1" | bc -l)
-battDecimal=$(echo "scale=2; $Vbatt/$VbattMax" | bc -l)
-battPercent=$(echo "$battDecimal * 100" | bc -l)
-battPercent=$(echo "scale=0; $battPercent / 1" | bc -l)
-
-# enable the battery LEDs
-/usr/bin/power-dock -q2
-
-# print output
-if [ $bJson == 1 ]; then
-	echo "{\"battery-voltage\":$VbattRounded}"
+if [ $progLoop -eq 0 ]; then
+	## normal operation - read value once then exit
+	reportBatteryLevel
 else
-	echo "Battery Voltage Level: $VbattRounded V"
-	#echo "Battery Level Percent: $battPercent %"
+	## loop operation - infinite loop to report battery level
+	##		time between reports is specified by progLoop (seconds)
+	while [ 1 ]; do
+		reportBatteryLevel
+		sleep $progLoop
+	done
 fi
